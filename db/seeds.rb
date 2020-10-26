7c42d0bdaf216042f9e1ebab4e3ad03ce5d328d7
Charlie Yang <seung.yang@mail.utoronto.ca>
2020-10-25 22:07:19 -0400

    adding in social media icons

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: "cyang1130@gmail.com", password: "112233")
+User.create(email: "hopefulchapteroflife@gmail.com", password: "pharmacy2020")
